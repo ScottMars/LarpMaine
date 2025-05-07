59e05facb89d1de459b050fe7075dad9c6b16bba
Sergey Spirenkov <91003279+ScottMars@users.noreply.github.com>
2025-05-08 00:08:40 +0300

    Update js/index.js

--- js/index.js
+++ js/index.js
@@ -5,17 +5,17 @@ export const vote_data = [
         title: "Greenland Acquisition Will Greenland become the 51st state… or remain the crown jewel of Denmark?",
         left: {
             image: "public/images/vote/ping.png",
-            name: "Yes, US will acquire Greenland",
+            name: "No, Denmark will hold the ice",
         },
         right: {
             image: "public/images/vote/trump.png",
-            name: "No, Denmark will hold the ice",
+            name: "Yes, US will acquire Greenland",
         },
-        start_date: new Date(2025, 4, 8, 0, 00),
-        end_date: new Date(2025, 4, 8, 18, 00),
+        start_date: new Date(2025, 4, 8, 0, 0),
+        end_date: new Date(2025, 4, 8, 18, 0),
         // end_date: new Date(2025, 4, 1, 9, 46),
         winner: "left",
-        duration: new Date(2025, 4, 8, 23, 59),
+        duration: new Date(2025, 4, 9, 0, 0),
         // duration: new Date(2025, 4, 1, 12),
         choices: {
             yes: 7222,
